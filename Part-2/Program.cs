ffe467c71cde0091056a4e546a45fd6a2ea88bfe
Dao Thai Ha <daothaiha1692005@gmail.com>
2024-12-15 01:48:03 +0200

    Add project files.

---
+++ Part-2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Part_2
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            List<Sensor> sensors = new List<Sensor>();
+
+            // Add instances to the list
+            sensors.Add(new DoorSensor(123456, true));
+            sensors.Add(new DoorSensor(654321, false));
+            sensors.Add(new TemperatureSensor(987654, 22.5));
+            sensors.Add(new TemperatureSensor(456789, 18.7));
+            sensors.Add(new DoorSensor(567890, true));
+            sensors.Add(new TemperatureSensor(345678, 25.3));
+
+            // Sort the element of list based on serial
+            sensors.Sort();
+
+            // Iterate through the sorted list and print 
+            foreach (var sensor in sensors)
+            {
+                Console.WriteLine(sensor.ToString());
+            }
+        }
+    }
+}
---
+++ Part-2/Sensor.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_2
+{
+    public abstract class Sensor : IComparable<Sensor>
+    {
+        // Serial property
+        public int _serial;
+        public int Serial
+        {
+            get { return _serial; }
+            set
+            {
+                if (999999 >= value && value >= 100000)
+                {
+                    _serial = value;
+                }
+                else
+                {
+                    _serial = -1;
+                }
+            }
+        }
+
+        // Initialize constructor method
+        public Sensor(int serial)
+        {
+            Serial = serial;
+        }
+
+        //Abstract CurrentStatus method
+        public abstract string CurrentStatus();
+
+        // Override ToString method
+        public override string ToString()
+        {
+            return $"Serial of sensor: {Serial}";
+        }
+
+        // Implement IComparable: a way to define how objects are compared to each other
+        public int CompareTo(Sensor? other)
+        {
+            if (other == null) return 1;
+            return Serial.CompareTo(other.Serial);
+        }
+    }
+
+    public class DoorSensor : Sensor
+    {
+        public bool IsOpen { get; set; }
+
+        //Constructor
+        public DoorSensor(int serial, bool isOpen) : base(serial)
+        {
+            IsOpen = isOpen;
+        }
+
+        //Override CurrentStatus method
+        public override string CurrentStatus()
+        {
+            if (IsOpen)
+            {
+                return "Open";
+            }
+            else
+            {
+                return "Closed";
+            }
+        }
+
+        //Override ToString method
+        public override string ToString()
+        {
+            return base.ToString() + $" Current status: {CurrentStatus()}";
+        }
+    }
+
+    public class TemperatureSensor : Sensor
+    {
+        public double Temperature { get; set; }
+
+        public TemperatureSensor(int serial, double temperature) : base(serial)
+        {
+            Temperature = temperature;
+        }
+
+        public override string CurrentStatus()
+        {
+            return $"{Temperature} Celsius!";
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" Current temperature: {CurrentStatus()}";
+        }
+    }
+}
